26f79060d5919195d2d89873e94a30b693325b6e
Anthony McLamb <adylanmclamb@yahoo.com>
2023-10-25 23:46:04 -0400

    Update

--- backend/src/main/java/com/aiquizgenerator/core/generator/services/GeneratorService.java
+++
@@ -1,9 +0,0 @@
-package com.aiquizgenerator.core.generator.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GeneratorService {
-
-
-}
--- backend/src/main/java/com/aiquizgenerator/core/openai/exceptions/NoResponsesFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aiquizgenerator.core.openai.exceptions;
-
-public class NoResponsesFoundException extends RuntimeException {
-    public NoResponsesFoundException() {
-        super("No responses found from openai");
-    }
-}
